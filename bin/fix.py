6e5288d7db9c9bfaddafb1e4fcd2f3981fea9274
Pietro <donapieppo@yahoo.it>
2023-04-06 16:50:39 +0200

    todo fix

--- bin/fix.py
+++ bin/fix.py
@@ -2,37 +2,37 @@
 
 import json
 import sqlite3
-from wikidata_db_reconciliation import WDHuman, WDItem, check_if_human
+from wikidata_local_reconciliation import WDHuman, WDItem, check_if_human
 
 connection = sqlite3.connect("/home/backup/wd.db")
 connection.row_factory = sqlite3.Row
-cursor = connection.cursor()
-cursor2 = connection.cursor()
+cursor_human = connection.cursor()
+cursor_wditem = connection.cursor()
 
-cursor.execute("SELECT * from humans")
+cursor_human.execute("SELECT * from humans")
 
-for row in cursor:
-    for x in json.loads(row['labels']):
-        print(x)
-        cursor2.execute("""
-          INSERT INTO names (human_id, name, wiki_id) VALUES (?, ?, ?)
-          """, (row['id'], x, row['wiki_id']))
-    for x in json.loads(row['aliases']):
-        print(x)
-        cursor2.execute("""
-          INSERT INTO names (human_id, name, wiki_id) VALUES (?, ?, ?)
-          """, (row['id'], x, row['wiki_id']))
-    continue
-
-    print("------")
+for row in cursor_human:
+    print(f"--- row {row['id']} ----")
     print(row['qnames'])
-    if row['qnames'] == 'null':
-        continue
-    for qname in json.loads(row['qnames']):
-        cursor2.execute("SELECT * from wditems where wiki_id = ?", (qname,))
-        res = cursor2.fetchone()
-        if res and len(res) > 0:
-            print(res['labels'])
+    print(row['qsurnames'])
+    print("--- end row ----")
+    total = []
+    n = [] if row['qnames'] == '[null]' else json.loads(row['qnames'])
+    s = [] if row['qsurnames'] == '[null]' else json.loads(row['qsurnames'])
+    if n:
+        print(n)
+        total += n
+    if s:
+        print(s)
+        total += s
+    print(total)
+    print("----")
+    for qname in total:
+        cursor_wditem.execute("SELECT * from wditems where wiki_id = ?", (qname,))
+        name = " ".join([item['labels'] for item in cursor_wditem])
+        print(name)
+        # if res and len(res) > 0:
+    input(".")
 
-connection.commit()
-connection.close()
+#connection.commit()
+#connection.close()
